8236a40c5b58ec7a827efed29d466fd6a80dbad3
jerixmx <jerixmx@gmail.com>
2021-04-06 17:29:18 +0800

    Working Prime Checking function.

---
+++ Hackerrank/Prime/optimizedprime.cpp
@@ -0,0 +1,65 @@
+#include <cmath>
+#include <cstdio>
+#include <vector>
+#include <iostream>
+#include <algorithm>
+using namespace std;
+
+//divisibility test: 2,3,5,7
+
+bool isPrime(int n)
+{
+  if (n==1)
+  {
+    return false;
+  }
+  if(n == 2 || n == 3)
+  {
+    return true;
+  }
+  if(n % 2 == 0 || n % 3 == 0)
+  {
+    return false;
+  }else
+  {
+    int sqrtn=sqrt(n);
+    int max_k=n/6;
+    if(sqrtn/6>1)
+    {
+      max_k = sqrtn/6;
+    }
+    for(int k = 1;k < max_k;k++)
+    {
+      if(n%(6*k-1) == 0)
+      {
+        return false;
+      }else if(n%(6*k-1) == 0)
+      {
+        return false;
+      }
+    }
+  }
+
+  return true;
+}
+
+
+
+int main() {
+
+    int T = 0;
+    cin >> T;
+    int num=0;
+    vector<int> n;
+
+    for(int i = 0; i < T ; i++)
+    {
+      cin >> num;
+      n.push_back(num);
+    }      
+    for(int i = 0; i < T ; i++)
+    {
+          isPrime(n[i]) ? cout << "Prime\n" : cout <<"Not Prime\n";
+    }
+
+}
