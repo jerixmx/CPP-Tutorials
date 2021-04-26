944d45daf010982b28d5e541395193b20b1a73f1
jerixmx <jerixmx@gmail.com>
2021-04-26 13:38:39 +0800

    Added function declarations.

--- CPP Google for Education/artillery/functions.h
+++ CPP Google for Education/artillery/functions.h
@@ -13,8 +13,23 @@ const float pi = 3.14159265;
 const int max_distance = 1242;
 const int min_distance = 43;
 
+void StartUp();
+
+int EnemyDistance();
+
+bool OutOfBounds(float return_var, std::string min_max, float limit);
+float GetAngle(std::string Askphrase, std::string min_max, float limit);
+
+float DegToRadians(float degrees);
+float TimeInAir (float angle);
+float PlayerDistance (float angle, float time_in_air);
+
+void Shoot (int enemy_distance, int &difference, int &shots);
+void IfMissed (int difference, int shots);
+void NotifyHit (int shots, int &kills);
+
+void AskNewRound (char &yesno, int &shots);
+
+int Fire (int kills);
 
 char AskRepeat(std::string Askphrase);
-void StartUp();
-int EnemyDistance();
-int Fire (int kills);
